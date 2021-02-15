bffb9a542165bccfe47693c6bf010276219d2e13
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-15 15:32:59 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/*
+*This Java program simply outputs “Hello, World!”
+*/
+class Hello
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("Hello, World!");
+    }
+}
